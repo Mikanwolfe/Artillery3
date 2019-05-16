047230748322a13819eeeff3934a5f3ae7f12e4a
Mikan <ivory@nekox.net>
2019-05-16 10:14:35 +1000

    moving files to legacy folder

--- legacy/GameMain.cs
+++ legacy/GameMain.cs
@@ -1,15 +1,15 @@
-﻿using System;
+using System;
 
 namespace ArtillerySeries.src
 {
     public class GameMain
     {
-
+        
 
         public static void Main()
         {
             ArtilleryGame Artillery3 = new ArtilleryGame();
-            //Artillery3.Run();
+            Artillery3.Run();
         }
     }
 }
