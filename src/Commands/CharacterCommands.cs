e835cfd542deb8b4488254e96ff7c50cffed435e
Mikan <ivory@nekox.net>
2019-05-25 17:50:06 +1000

    commands reimplemented

---
+++ src/Commands/CharacterCommands.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtillerySeries.src
+{
+    public class ChargeWeaponCommand
+    : Command
+    {
+        public ChargeWeaponCommand()
+            : base("charge_weapon")
+        {
+        }
+
+        public override void Execute(A3RData a3RData)
+        {
+            a3RData.SelectedPlayer.Character.ChargeWeapon();
+        }
+    }
+    public class DepressWeaponCommand
+        : Command
+    {
+        public DepressWeaponCommand()
+            : base("depress_weapon")
+        {
+        }
+        public override void Execute(A3RData a3RData)
+        {
+            a3RData.SelectedPlayer.Character.DepressWeapon();
+        }
+    }
+
+    public class ElevateWeaponCommand
+        : Command
+    {
+        public ElevateWeaponCommand() 
+            : base("elevate_weapon")
+        {
+        }
+
+        public override void Execute(A3RData a3RData)
+        {
+            a3RData.SelectedPlayer.Character.ElevateWeapon();
+        }
+    }
+
+    public class FireWeaponCommand : Command
+    {
+        public FireWeaponCommand()
+            : base("fire_weapon")
+        {
+        }
+
+        public override void Execute(A3RData a3RData)
+        {
+            a3RData.SelectedPlayer.Character.FireWeapon();
+        }
+    }
+    public class MoveLeftCommand
+        : Command
+    {
+        public MoveLeftCommand()
+            : base("move_left")
+        {
+        }
+
+        public override void Execute(A3RData a3RData)
+        {
+            a3RData.SelectedPlayer.Character.MoveLeft();
+        }
+
+    }
+    public class MoveRightCommand : Command
+    {
+        public MoveRightCommand()
+            : base("move_right")
+        {
+        }
+
+        public override void Execute(A3RData a3RData)
+        {
+            a3RData.SelectedPlayer.Character.MoveRight();
+        }
+    }
+    public class SwitchWeaponCommand
+        : Command
+    {
+        public SwitchWeaponCommand()
+            : base("switch_weapon")
+        {
+        }
+        public override void Execute(A3RData a3RData)
+        {
+            a3RData.SelectedPlayer.Character.SwitchWeapon();
+        }
+    }
+}
--- src/Commands/Command.cs
+++ src/Commands/Command.cs
@@ -10,9 +10,19 @@ namespace ArtillerySeries.src
     public interface ICommand
     {
         void Execute(A3RData a3RData);
+        string Name { get; }
     }
     public abstract class Command : ICommand
     {
+        private string _name;
+
+        public Command(string name)
+        {
+            _name = name;
+        }
+
+        public string Name { get => _name; set => _name = value; }
+
         public abstract void Execute(A3RData a3RData);
     }
 }
